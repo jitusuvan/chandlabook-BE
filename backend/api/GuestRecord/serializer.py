5d21148e012b7d135ab999568ed4f77f9a21b59f
Chirag Chandratre <chirag.chandratre@humbingo.com>
2025-12-19 23:15:33 +0530

    Add guest name serialization and total calculations in GuestRecord view

--- backend/api/GuestRecord/serializer.py
+++ backend/api/GuestRecord/serializer.py
@@ -3,6 +3,12 @@ from rest_framework import serializers
 from .model import GuestRecord
 
 class GuestRecordSerializer(serializers.ModelSerializer):
+    guest_name = serializers.SerializerMethodField()
     class Meta:
         model = GuestRecord
         fields = "__all__"
+
+    def get_guest_name(self, obj):
+        if obj.guest:
+            return f"{obj.guest.first_name} {obj.guest.last_name or ''} {obj.guest.surname or ''}".strip()
+        return None
--- backend/api/GuestRecord/view.py
+++ backend/api/GuestRecord/view.py
@@ -1,6 +1,8 @@
 # views.py
 from rest_framework import viewsets, filters
+from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Sum, Q
 from .model import GuestRecord
 from .serializer import GuestRecordSerializer
 
@@ -11,3 +13,21 @@ class GuestRecordViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['guest', 'event', 'select']    # ?event=marriage
     search_fields = ['guest__first_name', 'guest__surname', 'event', 'select']
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        queryset = self.filter_queryset(self.get_queryset())
+        
+        aavel_total = queryset.filter(select='aavel').aggregate(Sum('amount'))['amount__sum'] or 0
+        mukel_total = queryset.filter(select='mukel').aggregate(Sum('amount'))['amount__sum'] or 0
+        difference = aavel_total - mukel_total
+        
+        # Add totals to results
+        totals = {
+            'aavel_total': aavel_total,
+            'mukel_total': mukel_total,
+            'difference': difference
+        }
+        response.data['results'] = [totals] + response.data['results']
+        
+        return response
