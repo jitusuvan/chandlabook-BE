9de6313e85d78ba1975626d26db9b3db1dc897b6
Jituoo7 <dev.jitendrasuvan@gmail.com>
2026-01-13 00:31:44 +0530

    "fixed-search""

--- backend/api/Event/view.py
+++ backend/api/Event/view.py
@@ -1,11 +1,15 @@
-from rest_framework import generics, status
+from rest_framework import generics, status, filters
 from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
 from .model import Event
 from .serializer import EventSerializer
 
 class EventListCreateView(generics.ListCreateAPIView):
     queryset = Event.objects.all().order_by('-date')
     serializer_class = EventSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['name', 'bride_groom_name']
+    filterset_fields = ['event_type', 'select_type']
 
 class EventDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Event.objects.all()
